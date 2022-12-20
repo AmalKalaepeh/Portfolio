9fbe72b78976fd7585ceb770a10118a82890b82c
Amal ka <93978463+AmalKalaepeh@users.noreply.github.com>
2022-12-21 01:50:48 +0700

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,1 +1,8 @@
-const header = document
+const header = document.querySelector("header");
+/* ----------------------Sticky NAvbar -----------------------*/
+
+function stickyNavbar(){
+header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+stickyNavbar();
+window.addEventListener("scroll", stickyNavbar);
