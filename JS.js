0296cd59c182d8e8a9ec37f23e96abdde7e66a27
Amal ka <93978463+AmalKalaepeh@users.noreply.github.com>
2022-12-21 15:47:58 +0700

    Add files via upload

--- JS.js
+++ JS.js
@@ -6,3 +6,6 @@ header.classList.toggle("scrolled", window.pageYOffset > 0);
 }
 stickyNavbar();
 window.addEventListener("scroll", stickyNavbar);
+/* ----------------------Reveal Animation -----------------------*/
+
+let sr = ScrollReveal().reveal('.headline');
